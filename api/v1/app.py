4520d5962380119ad477318f9a530706535f9119
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-06-11 21:30:14 +0200

    changed the way the documentation was

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Flask web application
+"""
+Flask web application
 """
 
 from flask import Flask, jsonify
@@ -8,22 +9,31 @@ from api.v1.views import app_views
 from os import getenv
 from flask_cors import CORS
 
+# creating an  instance of Flask
 app = Flask(__name__)
+
+# blue print app_views
 app.register_blueprint(app_views)
+
+# configure flask to Nice Json
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+# Enable CORS fo all routes, allowing access from any origin
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
 def teardown(error):
-    """Clean-up method
+    """
+    Clean-up method
     """
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
-    """Custom 404 error
+    """
+    Custom 404 error
     """
     return jsonify({'error': 'Not found'}), 404
 
